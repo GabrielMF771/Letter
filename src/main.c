368f47a81d971e39f16768ebd3615b823a5988c3
GabrielMF771 <gabrielmartinsf771@gmail.com>
2024-11-13 20:00:40 -0300

    OpenGL 3.0

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main() {
     if (!glfwInit()) {
@@ -23,7 +23,8 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 800, "Jogo", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 
+    600, "Jogo", NULL, NULL);
     if (window == NULL) {
         printf("Failed to create GLFW window\n");
         glfwTerminate();
@@ -49,6 +50,9 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
+
+        glfwSetKeyCallback(window, key_callback);
+
     }
 
     glfwTerminate();
@@ -69,4 +73,13 @@ void processInput(GLFWwindow* window){
         // Comando que a Janela deve fechar
         glfwSetWindowShouldClose(window, 1);
     }
+}
+//funcao retorno teclas do teclado
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_1 && action == GLFW_PRESS)
+        printf("Jogo iniciado\n");
+    if (key == GLFW_KEY_2 && action == GLFW_PRESS)
+        printf("Mostrando estatisticas\n");
 }
